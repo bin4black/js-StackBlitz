268423f16ef8bcbd146a1c407087641490db8bc7
bin4black <122944796+bin4black@users.noreply.github.com>
2023-01-18 02:06:20 +0100

    1

--- index.js
+++ index.js
@@ -12,3 +12,17 @@ let stocks = {
     holder : ["cone", "cup", "stick"],
     toppings : ["chocolate", "peanuts"],
  };
+
+ let order = (call_production) =>{
+
+    console.log("Order placed. Please call production")
+    
+    // function 👇 is being called 
+      call_production();
+    };
+    
+    let production = () =>{
+    
+    console.log("Production has started")
+    
+    };
