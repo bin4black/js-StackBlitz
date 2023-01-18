b059d763d4281e587fc4b63de7f543b64f7710df
bin4black <122944796+bin4black@users.noreply.github.com>
2023-01-18 02:05:34 +0100

    1

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 console.log("mi primer proyecto")
+
+let stocks = {
+    Fruits : ["strawberry", "grapes", "banana", "apple"],
+    liquid : ["water", "ice"],
+    holder : ["cone", "cup", "stick"],
+    toppings : ["chocolate", "peanuts"],
+ };
