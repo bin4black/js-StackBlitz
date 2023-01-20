ba8012cbb4caaedb349e9997f5cf37658c7826b6
bin4black <122944796+bin4black@users.noreply.github.com>
2023-01-20 01:14:20 +0100

    a

--- index.js
+++ index.js
@@ -30,12 +30,28 @@ let production = () =>{
 
   setTimeout(()=>{
     console.log("production has started")
-  },0)
+    setTimeout(()=>{
+      console.log("The fruit has been chopped")
+      setTimeout(()=>{
+        console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} Added`)
+        setTimeout(()=>{
+          console.log("start the machine")
+          setTimeout(()=>{
+            console.log(`Ice cream placed on ${stocks.holder[1]}`)
+            setTimeout(()=>{
+              console.log(`${stocks.toppings[0]} as toppings`)
+              setTimeout(()=>{
+                console.log("serve Ice cream")
+              },2000)
+            },3000)
+          },2000)
+        },1000)
+      },1000)
+    },2000)
+  },0000)
 
 };
 
 // Trigger 👇
 order(3, production);
 
-//nota fjjfjfjdd
-decodeURI
